50b54fa266958f1083688defb92389e7362b3e64
robertramos07281021 <robertramos07281021@gmail.com>
2023-06-10 08:58:17 +0800

    FEATURED UI

--- src/views/FeaturedRecipes.js
+++ src/views/FeaturedRecipes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import db from "../firebase-config";
+
 const FeaturedRecipes = () => {
 
     const [isfood, setFood] = useState([]);
@@ -46,155 +47,125 @@ const FeaturedRecipes = () => {
       const randomData = getRandomElements(isfood, 4);
 
 
-  // console.log(isfood);
-  return (<div className=' w-screen border flex justify-center pb-10'>
-        
-       <div className="">
-       <h1 className='text-3xl text-center py-5'>Featured Recipes</h1>
-       <hr></hr>
-         <div className='rounded-md flex flex-wrap w-[1000px] gap-5 justify-center  mt-5 '>
+  return (
+
+    <div className=' w-screen flex justify-center mt-5'>
+      <div className="">
+        <hr></hr>
+        <h1 className='text-3xl text-center font-bold sampleMenu pt-5'>Featured Recipes</h1>
+       
+        <div className='rounded-md flex flex-wrap w-[1000px] gap-5 justify-center  mt-10 '>
          
-             {randomData.map((food,index) => (
-               
-               <div key={index}>
-                 <div className='w-52 rounded overflow-hidden'>
-                   <img
-                     className='w-48 h-48' src={food.image} alt='food'/>
-                   <div className='px-6 py-3'>
-                     <div className='font-bold text-sm mb-2 text-center'>
-                       {food.foodName.toUpperCase()}
-                     </div>
-                   </div>
+            {randomData.map((food,index) => (
+             
+              <div key={index}>
+                <div className='w-52 rounded'>
+                  <img
+                    className='w-52 h-48' src={food.image} alt='food'/>
+                  <div className='px-6 py-3'>
+                    <div className='font-bold text-sm mb-2 text-center'>
+                      {food.foodName.toUpperCase()}
+                    </div>
                  </div>
-                
-                 <div className="text-center mb-4">
-                   <button onClick={()=>
-                     toggleFood(food)
-                   } className="p-2 hover:bg-orange-600 hover:text-white hover: rounded-lg bg-orange-500 font-bold">Read More</button>
-                 </div>
+                </div>
+              
+                <div className="text-center mb-4">
+                 <button onClick={()=>
+                   toggleFood(food)
+                 } className="p-2 hover:bg-orange-600 hover:text-white hover: rounded-lg bg-orange-500 font-bold">Read More</button>
+                </div>
+                {showInfo && selectedFood === food && (
+                  <div className='fixed bg-slate-950/50 w-screen h-screen rounded drop-shadow-lg randomInfo'>
+                    <div className='p-5 inline-block w-9/12 h-[42rem] bg-orange-300 foodInfo mb-1 pt-12 overflow-auto pb-28'>
+                      <h1 className="text-4xl">{food.foodName.toUpperCase()}</h1>
+                      <hr></hr>
+                      <h3><stong>Category</stong></h3>
+                      <h3><strong>Ingredients</strong></h3>
+                      
+                      <div className="grid grid-cols-2">
+                        <div className="pl-5">
+                          <p><span className="font-medium">{food.foodIngredients}</span> </p>
+                        </div>
+                      </div>
+                      
+                      <div>
+                        <h3 className="pt-5">
+                          <strong>Instructions:</strong>
+                        </h3>
+                        <p className="indent-10 text-justify px-5">{food.foodSummary}</p>
+                        <h3 className="pt-5"><strong>Image:</strong></h3>
+                        <img className='w-52 h-48 ml-5 mt-3 rounded-lg'src={food.image} alt='Food'/>
+                        <button className=' absolute border border-black p-2 top-4 right-4 p-2 hover:bg-orange-600 hover:text-whitehover:rounded-lg bg-orange-500 font-bold'onClick={closePopup}>Close</button>
+                      </div>
+                      
+                    </div>
+                   
+                  </div>
+                )}
+              </div>
+            ))}
 
-                 {showInfo && selectedFood === food && (
-                   <div className='fixed bg-slate-950/50 w-screen h-screen rounded drop-shadow-lg randomInfo'>
-                     <div className='p-5 inline-block w-9/12 h-[42rem] bg-orange-300 foodInfo mb-1 pt-12 overflow-auto pb-28'>
-                       <h1 className="text-4xl">{food.foodName.toUpperCase()}</h1>
-                       <hr></hr>
-                       <h3>
-                         <strong>Ingredients</strong>
-                       </h3>
-                       <div className="grid grid-cols-2">
-                       <div className="pl-5">
-                         <p><span className="font-medium">{food.foodIngredients}</span> </p>
+             
+
+            {isOpenFeatured && 
+              <div className='fixed bg-slate-950/50 w-screen h-screen rounded drop-shadow-lg randomInfo'>
+                <div className='relative'>
+                  <div className='p-5 flex w-9/12 h-[42rem] bg-orange-300 foodInfo mb-1 pt-12 overflow-auto pb-28 gap-5'>
+
+                    <button className=' absolute p-2 top-4 right-4 p-2 hover:bg-orange-600 hover:text-white hover:   rounded-lg bg-orange-500     font-bold'  onClick={()=>{setOpenFeatured(false)}}>Close</button>
+                    {isfood.map((food) => (
+                      <div key={food.id}>
+                        <div className='w-52'>
+                          <img className='w-48 h-48 rounded-lg' src={food.image} alt='food'/>
+                          <div className='px-6 py-3'>
+                            <div className='font-bold text-sm mb-2 text-center'>
+                              {food.foodName.toUpperCase()}
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="text-center mb-4">
+                         <button onClick={()=>toggleFood(food)} className="p-2 hover:bg-orange-600 hover:text-white hover: rounded-lg bg-orange-500       font-bold">Read More</button>
+                        </div>
+                    
+                        {showInfo && selectedFood === food && (
+                          <div className='fixed bg-slate-950/50 w-screen h-screen rounded drop-shadow-lg randomInfo'>
+                            <div className='p-5 inline-block w-9/12 h-[42rem] bg-orange-300 foodInfo mb-1 pt-12 overflow-auto pb-28'>
                         
-                       </div>
-                   
-                     </div>
-                       <div>
-                       <h3 className="pt-5">
-                         <strong>Instructions:</strong>
-                       </h3>
-                       <p className="indent-10 text-justify px-5">{food.foodSummary}</p>
-                   
-                     
-
-                       <h3 className="pt-5">
-                         <strong>Image:</strong>
-                       </h3>
-                       <img
-                         className='w-80 h-80 ml-5 mt-3 rounded-lg'
-                         src={food.image}
-                         alt='Food'
-                       />
-                         <button
-                           className=' absolute border border-black p-2 top-4   right-4 p-2 hover:bg-orange-600 hover:text-white hover:   rounded-lg bg-orange-500 font-bold'
-                           onClick={closePopup}>
-                             {" "}
-                           Close
-                       </button>
-                       </div>
-                     </div>
-                     
-                   </div>
-                 )}
-               </div>
-             ))}
-        <br></br>
-             <button onClick={()=>{ setOpenFeatured(!isOpenFeatured)}}>See More</button>
-
-             { isOpenFeatured && <div className='absolute  bg-white  w-auto h-auto'><div className='relative'>
-             <button className='absolute right-2 top-0' onClick={()=>{setOpenFeatured(false)}}>Close</button>
-            
-            
-             <div className='flex m-20'>
-                
-                
-                {isfood.map((food) => (
-               
-               <div key={food.id}>
-                 <div className='w-52 rounded overflow-hidden'>
-                   <img
-                     className='w-48 h-48' src={food.image} alt='food'/>
-                   <div className='px-6 py-3'>
-                     <div className='font-bold text-sm mb-2 text-center'>
-                       {food.foodName.toUpperCase()}
-                     </div>
-                   </div>
-                 </div>
-                
-                 <div className="text-center mb-4">
-                   <button onClick={()=>
-                     toggleFood(food)
-                   } className="p-2 hover:bg-orange-600 hover:text-white hover: rounded-lg bg-orange-500 font-bold">Read More</button>
-                 </div>
-
-                 {showInfo && selectedFood === food && (
-                   <div className='fixed bg-slate-950/50 w-screen h-screen rounded drop-shadow-lg randomInfo'>
-                     <div className='p-5 inline-block w-9/12 h-[42rem] bg-orange-300 foodInfo mb-1 pt-12 overflow-auto pb-28'>
-                       <h1 className="text-4xl">{food.foodName.toUpperCase()}</h1>
-                       <hr></hr>
-                       <h3>
-                         <strong>Ingredients</strong>
-                       </h3>
-                       <div className="grid grid-cols-2">
-                       <div className="pl-5">
-                         <p><span className="font-medium">{food.foodIngredients}</span> </p>
+                              <h1 className="text-4xl">{food.foodName.toUpperCase()}</h1>
+                              <hr></hr>
+                              <h3><strong>Ingredients</strong></h3>
                         
-                       </div>
-                   
-                     </div>
-                       <div>
-                       <h3 className="pt-5">
-                         <strong>Instructions:</strong>
-                       </h3>
-                       <p className="indent-10 text-justify px-5">{food.foodSummary}</p>
-                   
-                     
-
-                       <h3 className="pt-5">
-                         <strong>Image:</strong>
-                       </h3>
-                       <img
-                         className='w-80 h-80 ml-5 mt-3 rounded-lg'
-                         src={food.image}
-                         alt='Food'
-                       />
-                         <button
-                           className=' absolute border border-black p-2 top-4   right-4 p-2 hover:bg-orange-600 hover:text-white hover:   rounded-lg bg-orange-500 font-bold'
-                           onClick={closePopup}>
-                             {" "}
-                           Close
-                       </button>
-                       </div>
-                     </div>
-                     
-                   </div>
-                 )}
-               </div>
-             ))}</div> </div> </div>  }
-           </div>
-          
-         </div>
-                   
-     </div>
+                              <div className="grid grid-cols-2">
+                                <div className="pl-5">
+                                  <p><span className="font-medium">{food.foodIngredients}</span> </p>
+                                </div>
+                              </div>
+                        
+                              <div>
+                                <h3 className="pt-5"><strong>Instructions:</strong></h3>
+                                <p className=" text-justify px-5">{food.foodSummary}</p>
+                                <h3 className="pt-5"><strong>Image:</strong></h3>
+                                <img className='w-80 h-80 ml-5 mt-3 rounded-lg'src={food.image} alt='Food'/>
+                                <button className=' absolute border border-black p-2 top-4 right-4 p-2 hover:bg-orange-600 hover:text-white hover:        rounded-lg bg-orange-500 font-bold' onClick={closePopup}>Close</button>
+                              </div>
+                        
+                            </div>
+                          </div>
+                        )}
+                      </div>
+                    ))}
+                  </div> 
+                </div> 
+              </div> 
+            }
+              
+        </div>
+        <div className='flex justify-center mt-10'>
+           <button onClick={()=>{ setOpenFeatured(!isOpenFeatured)}} className='p-2 hover:bg-orange-600 hover:text-white hover: rounded-lg     bg-orange-500 font-bold'>See More</button>
+        </div>
+      </div>            
+    </div>
   )
 }
 
