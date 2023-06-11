ab766442b0934b2ac8b10cca8ee993866dc47ea0
robertramos07281021 <robertramos07281021@gmail.com>
2023-06-11 11:23:41 +0800

    delete file

--- src/views/category.js
+++ src/views/category.js
@@ -12,24 +12,35 @@ export default function Category({ category }) {
   }, []);
 
   return (
-    <div >
+    <div className="" >
       <img onClick={()=> setIsOpen(true)} src={category.strCategoryThumb} alt="categories" className="rounded-lg"/>
       <div className="font-bold text-center" onClick={()=> setIsOpen(true)}>{category.strCategory}</div>
       
-     
-      
-      {categoryStage.map(catStage =>(
-        <div key={catStage.idMeal} className="justify-self-center w-56 text-center mt-2">
-          
-          <div className="flex justify-center">
-            <img src={catStage.strMealThumb} alt="Modal images" className="rounded-lg "/>
-            
+
+      {isOpen && 
+      <div className="fixed bg-slate-950/50 w-screen h-screen rounded drop-shadow-lg randomInfo">
+         <div className="p-5 inline-block w-9/12 h-[42rem] bg-orange-300 foodInfo mb-1 pt-12 overflow-auto pb-28">
+          <div>
+          <img src={category.strCategoryThumb} alt="categories" className="rounded-lg"/>
+          <h1>{category.strCategory}</h1>
           </div>
-          <p><strong>{catStage.strMeal}</strong></p>
-          <button onClick={()=>{setIsOpen(false)}} className='absolute border border-black p-2 top-20 right-60 p-2 rounded-lg hover:bg-orange-600 hover:text-white hover:rounded-lg bg-orange-500  font-bold'>Close</button>
-      
-        </div>
+
+          {categoryStage.map(catStage =>(
+          <div key={catStage.idMeal} className="justify-self-center w-56 text-center mt-2">
+
+            <div className="flex justify-center">
+              <img src={catStage.strMealThumb} alt="Modal images" className="rounded-lg "/>
+
+            </div>
+            <p><strong>{catStage.strMeal}</strong></p>
+            <button onClick={()=>{setIsOpen(false)}} className='fixed border border-black p-2 top-14 right-60 p-2 rounded-lg hover:bg-orange-600 hover:text-white hover:rounded-lg bg-orange-500  font-bold'>Close</button>
+        
+          </div>
+        
       ))}
+      </div>
+      </div>
+      }
     
     </div>
   )
